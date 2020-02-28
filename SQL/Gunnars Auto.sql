6dd3e153373700638920e6e8617add5374d38774
Asger <asge4900@edu.campusvejle.dk>
2020-02-28 11:43:46 +0100

    Add sql

---
+++ SQL/Gunnars Auto.sql
@@ -0,0 +1,35 @@
+USE master
+IF EXISTS(select * from sys.databases where name='GunnarsAutoDB')
+BEGIN
+alter database GunnarsAutoDB set single_user with rollback IMMEDIATE
+DROP DATABASE GunnarsAutoDB
+END
+
+CREATE DATABASE GunnarsAutoDB
+USE GunnarsAutoDB
+
+CREATE TABLE SalesPersons (
+	SalesPersonId int NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	FirstName nvarchar(100) NOT NULL,
+	LastName nvarchar(100) NOT NULL,
+	Initials nchar(4) NOT NULL,
+	Sales bit
+)
+
+CREATE TABLE Cars (
+	CarId int NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	Brand nvarchar(100) NOT NULL,
+	Model nvarchar(100) NOT NULL,
+	Vin nchar(17) NOT NULL,
+	RegisterNumber nvarchar(10),
+	IsUsed bit NOT NULL
+)
+
+CREATE TABLE Sales (
+	SalesId int NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	Sold bit NOT NULL,
+	SalesPerson int NOT NULL FOREIGN KEY REFERENCES SalesPersons(SalesPersonId),
+	Car int NOT NULL FOREIGN KEY REFERENCES Cars(CarId),
+	BuyPrice money NOT NULL,
+	SalesPrice money
+)

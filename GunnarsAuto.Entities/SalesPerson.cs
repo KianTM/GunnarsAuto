19278f51c1deff61b86b8f0058832d945b3a501d
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-02 11:32:47 +0100

    SalesPerson class created Properties added Tostring() overiden. No validation adeed yet.

---
+++ GunnarsAuto.Entities/SalesPerson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunnarsAuto.Entities
+{
+    public class SalesPerson
+    {
+        public int SalesPersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Initials { get; set; }
+        public bool Hired { get; set; }
+
+        public override string ToString()
+        {
+            string isHired = "nej";
+            if (Hired == true)
+            {
+                isHired = "ja";
+            }
+            return $"Fornavn: {FirstName}\nEfternavn: {LastName}\nInitialer: {Initials}\nAnsat: {isHired}";
+        }
+    }
+}

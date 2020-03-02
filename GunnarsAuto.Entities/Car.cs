af36dfbb75cb15a54565816c8ac50911009360eb
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-02 10:29:18 +0100

    Car class created properties filled out

---
+++ GunnarsAuto.Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunnarsAuto.Entities
+{
+    class Car
+    {
+        public int CarId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+        public string RegistreNumber { get; set; }
+        public bool IsUsed { get; set; }
+
+    }
+}

1a3a4d60bcd0dbab0ba4991ea970f4fae137d85e
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-03 09:34:28 +0100

    SalesPersonRepository created

---
+++ GunnarsAuto.DAL/SalesPersonRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using GunnarsAuto.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunnarsAuto.DAL
+{
+    class SalesPersonRepository
+    {
+        public SalesPersonRepository()
+        {
+
+        }
+
+        public List<SalesPerson> GetAll()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(helper.CnnVal("GunnarsAuto")))
+            {
+                return connection.Query<SalesPerson>("GetAllSalesPersons").ToList();
+            }
+        }
+
+        public Car GetSingleSalesPerson(int salesPersonId)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(helper.CnnVal("GunnarsAuto")))
+            {
+                return connection.Query<SalesPerson>("GetSalesPerson @CarId", salesPersonId) as Car;
+            }
+        }
+    }
+}

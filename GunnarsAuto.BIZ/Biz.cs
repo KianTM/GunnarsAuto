8395a4a221d96561aa27320d5fe6fd4c706d6f97
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-03 09:26:08 +0100

    SalesPerson methods added but not yet implemented. Sales methods added but not yet implemented.

--- GunnarsAuto.BIZ/Biz.cs
+++ GunnarsAuto.BIZ/Biz.cs
@@ -11,9 +11,11 @@ namespace GunnarsAuto.BIZ
     class Biz
     {
         CarRepository carRepository;
+        // SalesPersonRepository salesPersonRepository;
         public Biz()
         {
             carRepository = new CarRepository();
+            // salesPersonRepository = new SalesPersonRepository();
         }
 
         #region Cars
@@ -43,5 +45,53 @@ namespace GunnarsAuto.BIZ
         }
         #endregion
 
+        #region SalesPerson
+        public SalesPerson GetSingleSalesPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SalesPerson> GetAllSalesPersons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSalesPerson(SalesPerson person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSalesPerson(SalesPerson person)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Sales
+        public Sale GetSingleSale(int salesId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Sale> GetAllSales()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSale(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSale(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Sale> GetSalesBasedOnSalesPerson(int salesPersonId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
